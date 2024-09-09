b7fa0c727508b30da75d98e88e5616d0c2a64a13
Sailash2005 <s.sailash2005@gmail.com>
2024-09-09 06:13:24 +0530

    d

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const mongoose = require('mongoose');
 const PORT = 4000;
+
+app.use(express.static(path.join(__dirname,'public')))
 
 main().catch(err => console.log(err));
 
